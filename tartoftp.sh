ffbb8c21a6eb6fdcdca805e6954c2884c3be6793
Hugo Moragues <hugo.moragues@gmail.com>
2018-02-14 13:04:37 +0100

    fix PATH var name

--- tartoftp.sh
+++ tartoftp.sh
@@ -6,13 +6,13 @@ FECHA=`date +%Y-%m-%d`
 HOST='YOURHOSTNAME'
 USER='YOURFTPUSER'
 PASSWD='YOURFTPPASSWORD'
-PATH='THEPATHDESIREDTOBACKUP'
+BACKUPPATH='THEPATHDESIREDTOBACKUP'
 
 ftp -n $HOST << END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 binary
-put |"tar cvzf - $PATH" ./backups/tomcat/backup-$FECHA.tar.gz
+put |"tar cvzf - $BACKUPPATH" ./backups/tomcat/backup-$FECHA.tar.gz
 quit
 END_SCRIPT
 
